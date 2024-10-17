636f84f9c32001860958cd5c87996f10c93fa17f
felipe <130618161+fa9958189@users.noreply.github.com>
2024-10-17 15:36:29 -0300

    meu 2 commit

--- script.js
+++ script.js
@@ -7,26 +7,16 @@ const inputTransactionName = document.querySelector('#text')
 const inputTransactionAmount = document.querySelector('#amount')
 
 // Configura o armazenamento das transações no Local Storage
-// A const abaixo recebe o valor da chave transactions do Local Storage
-// ... e converte em um objeto JavaScript
-const localStorageTransactions = JSON.parse(localStorage
-  .getItem('transactions'))
-// A const abaixo verifica se a chave transactions do Local Storage
-// ... é diferente de nulo, para atribuir a variável acima ou, do contrário,
-// ... um array vazio
-let transactions = localStorage
-  .getItem('transactions') !== null ? localStorageTransactions : []
+const localStorageTransactions = JSON.parse(localStorage.getItem('transactions'))
+let transactions = localStorage.getItem('transactions') !== null ? localStorageTransactions : []
 
 const removeTransaction = ID => {
-  // Remove uma transação pelo ID e atualiza no Local Storage e na página
-  transactions = transactions.filter(transaction => 
-    transaction.id !== ID)
+  transactions = transactions.filter(transaction => transaction.id !== ID)
   updateLocalStorage()
   init()
 }
 
 const addTransactionIntoDOM = transaction => {
-  // Adiciona a transação no DOM
   const operator = transaction.amount < 0 ? '-' : '+'
   const CSSClass = transaction.amount < 0 ? 'minus' : 'plus'
   const amountWithoutOperator = Math.abs(transaction.amount)
@@ -40,26 +30,14 @@ const addTransactionIntoDOM = transaction => {
       x
     </button>
   `
-  // Não usar innerHTML, pois li é um objeto, e não uma string
-  // prepend insere como primeiro filho, append insere como último
   transactionUl.prepend(li)
 }
 
 const updateBalanceValues = () => {
-  // Atualiza o somatório de receitas e despesas e o saldo
-  const transactionsAmounts = transactions
-    .map(transaction => transaction.amount)
-  const total = transactionsAmounts
-    .reduce((accumulator, transaction) => accumulator + transaction, 0)
-    .toFixed(2)
-  const income = transactionsAmounts
-    .filter(value => value > 0)
-    .reduce((accumulator, value) => accumulator + value, 0)
-    .toFixed(2)
-  const expense = Math.abs(transactionsAmounts
-    .filter(value => value < 0)
-    .reduce((accumulator, value) => accumulator + value, 0))
-    .toFixed(2)
+  const transactionsAmounts = transactions.map(transaction => transaction.amount)
+  const total = transactionsAmounts.reduce((accumulator, transaction) => accumulator + transaction, 0).toFixed(2)
+  const income = transactionsAmounts.filter(value => value > 0).reduce((accumulator, value) => accumulator + value, 0).toFixed(2)
+  const expense = Math.abs(transactionsAmounts.filter(value => value < 0).reduce((accumulator, value) => accumulator + value, 0)).toFixed(2)
   
   balanceDisplay.textContent = `R$ ${total}`
   incomeDisplay.textContent = `R$ ${income}`
@@ -67,7 +45,6 @@ const updateBalanceValues = () => {
 }
 
 const init = () => {
-  // Executa o preenchimento na página
   transactionUl.innerHTML = ''
   transactions.forEach(addTransactionIntoDOM)
   updateBalanceValues()
@@ -76,17 +53,12 @@ const init = () => {
 init()
 
 const updateLocalStorage = () => {
-  // Atualiza a chave transactions do Local Storage para o array 
-  // ... de transações em forma de string
   localStorage.setItem('transactions', JSON.stringify(transactions))
 }
 
-// Gera um id aleatório
 const generateID = () => Math.round(Math.random() * 1000)
 
 form.addEventListener('submit', event => {
-  // Escuta o submit do form para verificar se inputs estão
-  // ... preenchidos e adicionar no array de transações
   event.preventDefault()
 
   const transactionName = inputTransactionName.value.trim()
@@ -97,20 +69,16 @@ form.addEventListener('submit', event => {
     return
   }
 
-  // Cria o objeto
   const transaction = { 
     id: generateID(), 
     name: transactionName, 
     amount: Number(transactionAmount)
   }
 
-  // Adiciona no array de transações, preenche na página e 
-  // ... atualiza no Local Storage
   transactions.push(transaction)
   init()
   updateLocalStorage()
 
-  // Limpa os inputs
   inputTransactionName.value = ''
   inputTransactionAmount.value = ''
 })
